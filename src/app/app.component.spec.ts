8439dace14013b4da3977dade3cd65717483796f
fabriziocucina-uniandes <f.cucina@uniandes.edu.co>
2024-11-01 15:58:33 -0500

    finished testing component, punto 3 listo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ListarPlantaComponent } from './planta/components/listar-planta/listar-planta.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterModule.forRoot([])
+        RouterModule.forRoot([]),
+        HttpClientTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ListarPlantaComponent
       ],
     }).compileComponents();
   });
@@ -18,18 +22,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'practica-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('practica-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, practica-angular');
   });
 });
--- src/app/planta/components/listar-planta/listar-planta.component.spec.ts
+++ src/app/planta/components/listar-planta/listar-planta.component.spec.ts
@@ -1,28 +1,48 @@
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListarPlantaComponent } from './listar-planta.component';
+import { PlantaService } from '../../services/planta.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { Planta } from '../../models/planta.model';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('ListarPlantaComponent', () => {
   let component: ListarPlantaComponent;
   let fixture: ComponentFixture<ListarPlantaComponent>;
+  let plantaService: PlantaService;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ListarPlantaComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [ListarPlantaComponent],
+      providers: [PlantaService]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarPlantaComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    plantaService = TestBed.inject(PlantaService);
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create the table with three rows', () => {
+
+    const mockPlantas: Planta[] = [
+      new Planta(1, "Lengua de vaca", "Sansevieria Trifasciata", "Interior", 140, "Templado, cálido", "Tierra orgánica con buen drenaje"),
+      new Planta(2, "Chachafruto", "Schefflera actinophylla", "Exterior", 1000, "Todos", "Sustrato para huerto"),
+      new Planta(3, "Pothos", "Epipremnum aureum", "Interior", 300, "Tropical", "Tierra con buen drenaje")
+    ];
+
+    spyOn(plantaService, 'getPlants').and.returnValue(of(mockPlantas));
+
+    component.getPlants();
+    fixture.detectChanges();
+
+    const rows = fixture.nativeElement.querySelectorAll('tbody tr');
+    expect(rows.length).toBe(3);
   });
 });
